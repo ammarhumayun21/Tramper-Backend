94e0ded805274753d477ed169298421cf9c71c27
Nouman Ejaz <noumanejaz92@gmail.com>
2026-02-03 12:29:03 +0500

    fix deploy

--- config/settings.py
+++ config/settings.py
@@ -5,3 +5,14 @@ Loads appropriate settings based on DJANGO_SETTINGS_MODULE environment variable.
 Default: config.settings.local (development)
 Production: config.settings.production
 """
+
+import os
+
+# Determine which settings to use
+ENV = os.environ.get('DJANGO_ENV', 'local')
+
+if ENV == 'production':
+    from .production import *  # noqa
+else:
+    from .local import *  # noqa
+
